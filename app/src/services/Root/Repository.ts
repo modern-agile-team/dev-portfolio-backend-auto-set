d30faf56d35aba420d7d93a4c1adeb9242b7f17f
김지수 <kimjisu3817@gmail.com>
2022-03-24 19:42:02 +0900

    Change the Repository file related to db connection

--- app/src/services/Root/Repository.ts
+++ app/src/services/Root/Repository.ts
@@ -4,10 +4,20 @@ export default class Repository {
   private pool;
 
   constructor() {
-    this.pool = publicdb.getConnection();
+      this.pool =  publicdb.getConnection();
+  }
+
+  async init(): Promise<void> {
+    await this.pool;
+  }
+
+  static async build(): Promise<any> {
+    const repository = new Repository();
+    await repository.init();
+    return repository;
   }
 
   async releaseConnection(): Promise<void> {
-    (await this.pool).release();
+    (await this.pool).release()
   }
 }
