109bc1616b4708e07a0c7045f0b36ac1857643a1
woorim960 <woorimprog@gmail.com>
2022-03-28 21:28:01 +0900

    Refactor to process the Errors

--- app/src/apis/TeckStacks/TeckStackController.ts
+++ app/src/apis/TeckStacks/TeckStackController.ts
@@ -4,10 +4,15 @@ import TeckStackRepository from '../../services/TeckStacks/TeckStackRepository';
 
 const TeckStackController = {
   findAllByKeyword: async (req: Request, res: Response) => {
-    const teckStackService = new TackStackService(req, await TeckStackRepository.build());
-    await teckStackService.findAllByKeyword();
+    try {
+      const teckStackService = new TackStackService(req, await TeckStackRepository.build());
+      await teckStackService.findAllByKeyword();
 
-    return res.status(200).json();
+      return res.status(200).json();
+    } catch (err) {
+      if (err instanceof Error) return { success: false, msg: err.message };
+      return {};
+    }
   },
 };
 
--- app/src/services/TeckStacks/TeckStackService.ts
+++ app/src/services/TeckStacks/TeckStackService.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line max-classes-per-file
 import { Request } from 'express';
 
 interface Query {
@@ -6,6 +7,7 @@ interface Query {
 
 export default class TackStackService {
   private teckStackRepository: any;
+
   query: any;
 
   constructor(readonly req: Request, teckStackRepository: any) {
@@ -14,21 +16,19 @@ export default class TackStackService {
   }
 
   async findAllByKeyword() {
-    try {
-      const keyword: Query = this.query.keyword;
+    const { keyword } = this.query;
 
-      if (keyword === undefined) {
-        return { success: false, msg: 'keyword is undefined' };
-      }
-      const tackStacks: [] = await this.teckStackRepository.findAllByKeyword();
-
-      if (!tackStacks.length) {
-        return { success: false, msg: `There aren't tackstacks you finded` };
-      }
-
-      return { success: true, tackStacks };
-    } catch (err) {
-      return { isError: true, success: false, msg: err };
+    if (keyword === undefined) {
+      throw new Error('keyword is undefined');
+      // return { success: false, msg: 'keyword is undefined' };
     }
+    const tackStacks: [] = await this.teckStackRepository.findAllByKeyword();
+
+    if (!tackStacks.length) {
+      throw new Error(`There aren't tackstacks you finded`);
+      // return { success: false, msg: `There aren't tackstacks you finded` };
+    }
+
+    return { success: true, tackStacks };
   }
 }
