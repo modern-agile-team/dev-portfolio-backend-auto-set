8916cc33d3ca5546e279b173eaa6e0449e06f5ed
woorim960 <woorimprog@gmail.com>
2022-03-28 20:40:02 +0900

    Delete a useless constructor of TechStackRepository

--- app/src/services/TeckStacks/TeckStackRepository.ts
+++ app/src/services/TeckStacks/TeckStackRepository.ts
@@ -1,10 +1,6 @@
 import Repository from '../Root/Repository';
 
 export default class TeckStackRepository extends Repository {
-  constructor() {
-    super();
-  }
-
   async findAllByKeyword(): Promise<[]> {
     const tackStacks: [] = [];
 
