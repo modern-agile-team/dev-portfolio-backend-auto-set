9f5745ce0cfe07c9b9d36fe5e4e3a6c0038d5752
woorim960 <woorimprog@gmail.com>
2022-03-30 12:43:29 +0900

    Fix a Response Interface Issue

--- app/src/apis/TechStacks/TechStackController.ts
+++ app/src/apis/TechStacks/TechStackController.ts
@@ -7,14 +7,14 @@ export interface CtrlResponse extends Response {
 }
 
 const TeckStackController = {
-  findAllByKeyword: async (req: Request, res: CtrlResponse, next: NextFunction) => {
+  findAllByKeyword: async (req: Request, res: Response, next: NextFunction) => {
     try {
       const teckStackService = new TachStackService(req, await TechStackRepository.build());
       await teckStackService.findAllByKeyword();
 
       res.status(200).json();
     } catch (err) {
-      res.err = err;
+      (res as CtrlResponse).err = err;
       next();
     }
   },
