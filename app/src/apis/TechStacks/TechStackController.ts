2d7153ae99b3767eaaeda1937222581d6298b440
woorim960 <woorimprog@gmail.com>
2022-03-31 21:10:59 +0900

    Delete useless console lines

--- app/src/apis/TechStacks/TechStackController.ts
+++ app/src/apis/TechStacks/TechStackController.ts
@@ -4,12 +4,10 @@ import TechStackRepository from '../../services/TechStacks/TechStackRepository';
 
 const TeckStackController = {
   findAllByKeyword: async (req: Request, res: Response, next: NextFunction) => {
-    console.log(1);
     try {
       const teckStackService = new TachStackService(req, await TechStackRepository.build());
-      console.log(2);
       await teckStackService.findAllByKeyword();
-      console.log(3);
+
       res.status(200).json();
     } catch (err) {
       res.err = err;
