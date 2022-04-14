2bd7445b277c64c9026e9be0cd4a489cea90f43d
woorim960 <woorimprog@gmail.com>
2022-04-14 12:59:29 +0900

    Init a contact service

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AdminsModule } from './admins/admins.module';
 import { ContactController } from './contact/contact.controller';
+import { ContactService } from './contact/contact.service';
 
 @Module({
   imports: [AdminsModule],
   controllers: [ContactController],
+  providers: [ContactService],
 })
 export class AppModule {}
