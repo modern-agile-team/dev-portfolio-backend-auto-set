6da3ac25c09f15107f0b9c115764b6aa9b907b08
woorim960 <woorimprog@gmail.com>
2022-04-14 12:00:29 +0900

    Update parser location to access tscconfig.json from anywhere

--- nest/.eslintrc.js
+++ nest/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    project: `${__dirname}/tsconfig.json`,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
