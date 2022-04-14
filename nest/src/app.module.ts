7a3929c23eb13ce8dde0a6459796b175ced4657c
김지수 <kimjisu3817@gmail.com>
2022-04-14 20:13:06 +0900

    Apply environment variables to db environment settings

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AdminsModule } from './admins/admins.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './configs/typeorm.config';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
-    TypeOrmModule.forRoot(typeOrmConfig),
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [`${__dirname}/../**/*.entity.{js,ts}`],
+      synchronize: false,
+    }),
     AdminsModule,
   ],
 })
