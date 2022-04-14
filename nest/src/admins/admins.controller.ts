df1caa8bfd25e45989987a5f966fb687e9734a6f
김지수 <kimjisu3817@gmail.com>
2022-04-14 11:53:48 +0900

    Apply DTO to admin controller and service files

--- nest/src/admins/admins.controller.ts
+++ nest/src/admins/admins.controller.ts
@@ -1,12 +1,13 @@
-import { Body, Controller, Post} from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AdminsService } from './admins.service';
+import { CheckAdminDto } from './dto/check-admin.dto';
 
 @Controller('admins')
 export class AdminsController {
   constructor(private adminsService: AdminsService) {}
 
   @Post()
-  login(@Body()) {
-    const await = this.adminsService.login(request);
+  login(@Body() loginInfo: CheckAdminDto) {
+    const await = this.adminsService.login(loginInfo);
   }
 }
