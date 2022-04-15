4e19c5b31345dce9305fabd29b4ea478c4bd9dcf
woorim960 <woorimprog@gmail.com>
2022-04-15 18:21:43 +0900

    Update all attributes's name from snake_case to camelCase

--- db/initdb.d/create_table.sql
+++ db/initdb.d/create_table.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS `admins` (
 	`email`	VARCHAR(60)	NOT NULL,
 	`password`	CHAR(64)	NOT NULL	COMMENT 'SHA-256 알고리즘, 길이 고정 64자',
 	`salt`	CHAR(10)	NOT NULL	COMMENT '길이 고정 10자',
-	`created`	TIMESTAMP	NOT NULL	DEFAULT CURRENT_TIMESTAMP(),
+	`createdAt`	TIMESTAMP	NOT NULL	DEFAULT CURRENT_TIMESTAMP(),
 
 	PRIMARY KEY (`email`)
 );
@@ -13,23 +13,23 @@ CREATE TABLE IF NOT EXISTS `admins` (
 CREATE TABLE IF NOT EXISTS `headers` (
 	`no`	INT UNSIGNED	NOT NULL	AUTO_INCREMENT	COMMENT '일련 번호',
 	`title`	VARCHAR(30)	NOT NULL,
-	`logo_url`	VARCHAR(255)	NOT NULL,
+	`logoUrl`	VARCHAR(255)	NOT NULL,
 
 	PRIMARY KEY (`no`)
 );
 
 CREATE TABLE IF NOT EXISTS `channels_in_header` (
 	`no`	INT UNSIGNED	NOT NULL	AUTO_INCREMENT	COMMENT '일련 번호',
-	`header_no`	INT UNSIGNED	NOT NULL,
+	`headerNo`	INT UNSIGNED	NOT NULL,
 	`name`	VARCHAR(20)	NOT NULL,
 	`url`	VARCHAR(255)	NOT NULL,
 
 	PRIMARY KEY (`no`),
-  FOREIGN KEY (`header_no`)
+  FOREIGN KEY (`headerNo`)
   REFERENCES `headers` (`no`) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS `tech_stacks` (
+CREATE TABLE IF NOT EXISTS `techStacks` (
 	`no`	INT UNSIGNED	NOT NULL	AUTO_INCREMENT	COMMENT '일련 번호',
 	`name`	VARCHAR(20)	NOT NULL,
 	`gauge`	INT	NOT NULL,
@@ -40,38 +40,38 @@ CREATE TABLE IF NOT EXISTS `tech_stacks` (
 CREATE TABLE IF NOT EXISTS `contacts` (
 	`no`	INT UNSIGNED	NOT NULL	AUTO_INCREMENT	COMMENT '일련 번호',
 	`title`	VARCHAR(20)	NOT NULL,
-	`sub_title`	VARCHAR(50)	NOT NULL,
-	`button_text`	VARCHAR(20)	NOT NULL,
+	`subTitle`	VARCHAR(50)	NOT NULL,
+	`buttonText`	VARCHAR(20)	NOT NULL,
 
 	PRIMARY KEY (`no`)
 );
 
-CREATE TABLE IF NOT EXISTS `channels_in_contact` (
+CREATE TABLE IF NOT EXISTS `contactChannels` (
 	`no`	INT UNSIGNED	NOT NULL	AUTO_INCREMENT	COMMENT '일련 번호',
-	`contact_no`	INT UNSIGNED	NOT NULL,
+	`contactNo`	INT UNSIGNED	NOT NULL,
 	`name`	VARCHAR(20)	NOT NULL,
 	`url`	VARCHAR(255)	NOT NULL,
 
 	PRIMARY KEY (`no`),
-  FOREIGN KEY (`contact_no`)
+  FOREIGN KEY (`contactNo`)
   REFERENCES `contacts` (`no`) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS `user_infos` (
 	`no`	INT UNSIGNED	NOT NULL	AUTO_INCREMENT	COMMENT '일련 번호',
-	`contact_no`	INT UNSIGNED	NOT NULL,
+	`contactNo`	INT UNSIGNED	NOT NULL,
 	`title`	VARCHAR(40)	NOT NULL,
 	`description`	VARCHAR(100)	NOT NULL,
 
 	PRIMARY KEY (`no`),
-  FOREIGN KEY (`contact_no`)
+  FOREIGN KEY (`contactNo`)
   REFERENCES `contacts` (`no`) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS `projects` (
 	`no`	INT UNSIGNED	NOT NULL	AUTO_INCREMENT	COMMENT '일련 번호',
 	`title`	VARCHAR(30)	NOT NULL,
-	`sub_title`	VARCHAR(100)	NULL,
+	`subTitle`	VARCHAR(100)	NULL,
 	`description`	TEXT	NULL	COMMENT '65535자 이하',
 
 	PRIMARY KEY (`no`)
@@ -87,31 +87,31 @@ CREATE TABLE IF NOT EXISTS `short_introductions` (
 
 CREATE TABLE IF NOT EXISTS `histories` (
 	`no`	INT UNSIGNED	NOT NULL	AUTO_INCREMENT	COMMENT '일련 번호',
-	`start_date`	DATE	NOT NULL,
-	`end_date`	DATE	NULL,
+	`startDate`	DATE	NOT NULL,
+	`endDate`	DATE	NULL,
 	`description`	VARCHAR(100)	NOT NULL,
-	`progress_flag`	TINYINT(1)	NOT NULL	COMMENT '0: 끝남 1: 진행중',
+	`progressFlag`	TINYINT(1)	NOT NULL	COMMENT '0: 끝남 1: 진행중',
 
 	PRIMARY KEY (`no`)
 );
 
 CREATE TABLE IF NOT EXISTS `careers` (
 	`no`	INT UNSIGNED	NOT NULL	AUTO_INCREMENT	COMMENT '일련 번호',
-	`start_date`	DATE	NOT NULL,
-	`end_date`	DATE	NULL,
+	`startDate`	DATE	NOT NULL,
+	`endDate`	DATE	NULL,
 	`description`	VARCHAR(100)	NOT NULL,
-	`progress_flag`	TINYINT(1)	NOT NULL	COMMENT '0: 끝남 1: 진행중',
+	`progressFlag`	TINYINT(1)	NOT NULL	COMMENT '0: 끝남 1: 진행중',
 
 	PRIMARY KEY (`no`)
 );
 
 CREATE TABLE IF NOT EXISTS `visitor_comments` (
 	`no`	INT UNSIGNED	NOT NULL	AUTO_INCREMENT	COMMENT '일련 번호',
-	`visitor_nickname`	VARCHAR(20)	NULL	DEFAULT "익명",
+	`visitorNickname`	VARCHAR(20)	NULL	DEFAULT "익명",
 	`password`	VARCHAR(30)	NOT NULL,
 	`description`	VARCHAR(500)	NOT NULL,
-	`created`	TIMESTAMP	NOT NULL	DEFAULT CURRENT_TIMESTAMP()	COMMENT '현재 시간',
-	`updated`	TIMESTAMP	NOT NULL	DEFAULT CURRENT_TIMESTAMP()	COMMENT '현재 시간, 처음 데이터는 생성일과 같다.',
+	`createdAt`	TIMESTAMP	NOT NULL	DEFAULT CURRENT_TIMESTAMP()	COMMENT '현재 시간',
+	`updatedAt`	TIMESTAMP	NOT NULL	DEFAULT CURRENT_TIMESTAMP()	COMMENT '현재 시간, 처음 데이터는 생성일과 같다.',
 
 	PRIMARY KEY (`no`)
 );
