eb47d1c4febf9ee4f49f1c0f754452d2aa8526eb
김지수 <kimjisu3817@gmail.com>
2022-04-19 22:27:00 +0900

    Create payload interface and implement create token

--- nest/src/auth/auth.service.ts
+++ nest/src/auth/auth.service.ts
@@ -8,4 +8,13 @@ export class AuthService {
   constructor() {
     this.secretKey = process.env.SECRET_KEY;
   }
+
+  async createToken(admin: Payload) {
+    const payload = { ...admin };
+
+    return jwt.sign(payload, this.secretKey, {
+      issuer: 'dev-portfolio',
+      expiresIn: '20h',
+    });
+  }
 }
