fb88cde3e6f2e1c1ba0b85cbc58d6a7b50c2b9f8
김지수 <kimjisu3817@gmail.com>
2022-04-21 11:42:23 +0900

    Create userconstant file for user environment value

--- nest/src/admins/admins.service.ts
+++ nest/src/admins/admins.service.ts
@@ -1,14 +1,15 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { AdminDto } from './dto/admin.dto';
+import { userConstants } from './constants';
 
 @Injectable()
 export class AdminsService {
   constructor(private jwtService: JwtService) {}
 
   login(adminDto: AdminDto): { token: string } {
-    const checkedEmail = process.env.ADMIN_EMAIL;
-    const checkedPassword = process.env.ADMIN_PSWORD;
+    const checkedEmail = userConstants.email;
+    const checkedPassword = userConstants.password;
     const { email, password } = adminDto;
 
     if (email !== checkedEmail || password !== checkedPassword)
---
+++ nest/src/admins/constants.ts
@@ -0,0 +1,4 @@
+export const userConstants = {
+  email: process.env.ADMIN_EMAIL,
+  password: process.env.ADMIN_PASSWORD,
+};
