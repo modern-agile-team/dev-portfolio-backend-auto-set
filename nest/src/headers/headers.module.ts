28cecc586a88a944797efb4f0b17da522e24effa
김지수 <kimjisu3817@gmail.com>
2022-04-28 00:35:35 +0900

    Import header and channel entity into headers module

--- nest/src/headers/headers.module.ts
+++ nest/src/headers/headers.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { HeadersService } from './headers.service';
 import { HeadersController } from './headers.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Header } from './entities/header.entity';
+import { Channel } from 'diagnostics_channel';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Header, Channel])],
   controllers: [HeadersController],
   providers: [HeadersService],
 })
