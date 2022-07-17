37328d79f45d5f235c4681291d7059f38b92004e
김지수 <kimjisu3817@gmail.com>
2022-07-17 17:59:54 +0900

    Chore: 라우팅 작업 추가

--- main.ts
+++ main.ts
@@ -4,11 +4,15 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+import techStack from './src/apis/tech-stack';
+
 app.listen(PORT, () => {
   console.log(`server start at ${PORT}`);
 });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/', techStack);
 
 export = app;
---
+++ src/apis/tech-stack/index.ts
@@ -0,0 +1,5 @@
+import * as express from 'express';
+
+const router: express.Router = express.Router();
+
+export default router;
