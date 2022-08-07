7a2fabada12387b64cdf8dbb2b67101dd9bef854
김지수 <kimjisu3817@gmail.com>
2022-08-07 22:20:17 +0900

    Modify: Modify swagger file path

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ import YAML from 'yamljs';
 import path from 'path';
 import 'dotenv/config';
 
-const swaggerSpec = YAML.load(path.join(__dirname, './build/swagger.yaml'));
+const swaggerSpec = YAML.load(path.join(__dirname, './swagger.yaml'));
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,10 +16,10 @@ app.listen(PORT, () => {
   console.log(`server start at ${PORT}`);
 });
 
-app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use('/api/header', header);
 app.use('/api/visitor', visitor);
