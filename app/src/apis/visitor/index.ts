bdadf7bca1cba127ecbcad1e33cf76a922acba0f
김지수 <kimjisu3817@gmail.com>
2022-09-12 14:33:12 +0900

    Add: Add validation check middleware

--- app/src/apis/visitor/index.ts
+++ app/src/apis/visitor/index.ts
@@ -1,13 +1,17 @@
 import * as express from 'express';
 import visitorCtrl from './visitor.ctrl';
-
+import validationCheck from '../middlewares/validationCheck';
 const router: express.Router = express.Router();
 
 router.patch('/count', visitorCtrl.updateAndGetVisitor);
 
-router.post('/comment', visitorCtrl.createVisitComment);
+router.post('/comment', validationCheck, visitorCtrl.createVisitComment);
 router.get('/comments', visitorCtrl.getVisitorComments);
-router.patch('/comment/:id', visitorCtrl.updateVisitCommentById);
 router.delete('/comment/:id', visitorCtrl.deleteVisitorCommentById);
+router.patch(
+  '/comment/:id',
+  validationCheck,
+  visitorCtrl.updateVisitCommentById
+);
 
 export default router;
--- app/src/apis/visitor/visitor.ctrl.ts
+++ app/src/apis/visitor/visitor.ctrl.ts
@@ -2,41 +2,28 @@ import { Request, Response } from 'express';
 import Visitor from '../../service/visitor';
 import VisitorRepository from '../../model/visitorRepository';
 import { BadRequestError } from '../../service/error';
-import { VisitorCmtDtoValidation, UpdateValidation } from './validationCheck';
-import { validate, ValidationError } from 'class-validator';
-import errorResposne from './error';
+import errorResposne from '../module/error';
 
 const updateAndGetVisitor = async (req: Request, res: Response) => {
   try {
-    const visitor = new Visitor(new VisitorRepository(), req.body);
+    const visitor = new Visitor(new VisitorRepository());
 
     const response = await visitor.updateAndGetVisitorCnt();
 
-    if (response) return res.status(200).json({ statusCode: 200, ...response });
+    return res.status(200).json({ statusCode: 200, ...response });
   } catch (err) {
     return errorResposne(err, res);
   }
 };
 
-const createVisitComment = async (req: Request, res: Response, next: any) => {
-  const { body } = req;
+const createVisitComment = async (req: Request, res: Response) => {
+  const RequestVisitorComment = Object.assign(req.body);
 
   try {
-    const visitorCmt = new VisitorCmtDtoValidation();
-    visitorCmt.nickname = body.nickname;
-    visitorCmt.password = body.password;
-    visitorCmt.description = body.description;
+    if (RequestVisitorComment.nickname?.length === 0)
+      req.body.nickname = '익명';
 
-    const validationError: ValidationError[] = await validate(visitorCmt);
-
-    if (validationError.length > 0) {
-      console.log(validationError);
-      throw new BadRequestError('The request data is malformed');
-    }
-
-    if (visitorCmt.nickname?.length === 0) body.nickname = '익명';
-
-    const visitor = new Visitor(new VisitorRepository(), body);
+    const visitor = new Visitor(new VisitorRepository(), RequestVisitorComment);
 
     const response = await visitor.createComment();
 
@@ -52,22 +39,12 @@ const createVisitComment = async (req: Request, res: Response, next: any) => {
 };
 
 const updateVisitCommentById = async (req: Request, res: Response) => {
-  const { body } = req;
   const { id: visitorCommentId } = req.params;
 
+  const requestVisitorComment = Object.assign(req.body);
+
   try {
-    const visitorCmt = new UpdateValidation();
-    visitorCmt.password = body.password;
-    visitorCmt.description = body.description;
-
-    const validationError: ValidationError[] = await validate(visitorCmt);
-
-    if (validationError.length > 0) {
-      console.log(validationError);
-      throw new BadRequestError('The request data is malformed');
-    }
-
-    const visitor = new Visitor(new VisitorRepository(), body);
+    const visitor = new Visitor(new VisitorRepository(), requestVisitorComment);
 
     const response = await visitor.updateCommentById(Number(visitorCommentId));
 
